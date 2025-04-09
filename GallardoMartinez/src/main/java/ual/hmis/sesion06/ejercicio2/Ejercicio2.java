4788d6dcfb821016e3a224b63b02eaff6fd9838c
Antonio Gallardo <agg860@inlumine.ual.es>
2025-04-09 19:51:39 +0200

    Commit antes de abrir nueva rama

--- GallardoMartinez/src/main/java/ual/hmis/sesion06/ejercicio2/Ejercicio2.java
+++ GallardoMartinez/src/main/java/ual/hmis/sesion06/ejercicio2/Ejercicio2.java
@@ -1,5 +1,40 @@
 package ual.hmis.sesion06.ejercicio2;
 
 public class Ejercicio2 {
+	
+	
+	public boolean login (String username, String password){
+		// comprobar que sean distintos de vacio
+		if(username.isEmpty() || password.isEmpty()) {
+			
+			return false; 
+		} 
+		
+		//Comprobar que ambos sean menores de 30
+		if(username.length() >= 30  || password.length() >= 30) {
+			
+			return false;
+			
+		}
+		
+		return compruebaLoginEnBD(username, password);
+		
+		}
+
+	
+		public boolean compruebaLoginEnBD
+		(String username, String password){
+		// método mock (simulado)
+		if (username.equals("user") && password.equals("pass"))
+		return true;
+		else
+		return false;
+		}
+	
+	
+	
+	
+	
+	
 
 }
--- GallardoMartinez/src/test/java/ual/hmis/sesion06/ejercicio2/Ejercicio2Test.java
+++ GallardoMartinez/src/test/java/ual/hmis/sesion06/ejercicio2/Ejercicio2Test.java
@@ -1,5 +1,9 @@
 package ual.hmis.sesion06.ejercicio2;
 
 public class Ejercicio2Test {
+	
+	
+	
+	
 
 }
