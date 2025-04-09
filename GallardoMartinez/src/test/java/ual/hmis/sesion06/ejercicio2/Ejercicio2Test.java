03b75a027c9d91e6d7fca475a3acac79490f4423
Antonio Gallardo <agg860@inlumine.ual.es>
2025-04-09 20:22:56 +0200

    commit rama 2

--- GallardoMartinez/src/test/java/ual/hmis/sesion06/ejercicio2/Ejercicio2Test.java
+++ GallardoMartinez/src/test/java/ual/hmis/sesion06/ejercicio2/Ejercicio2Test.java
@@ -1,6 +1,34 @@
 package ual.hmis.sesion06.ejercicio2;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import ual.hmis.sesion06.ejercicio1.Ejercicio1;
+
 public class Ejercicio2Test {
+	
+
+	//Arrange
+	@ParameterizedTest
+	@CsvSource({"'', contraseña, false",
+				"nombreUsuario, contraseña, false",
+				"user, pass, true",
+				})
+	
+	void testLogin(String nombreUsuario, String constraseña, Boolean resultado) {
+		
+		//Arrange: 
+		Ejercicio2 ejercicio2 = new Ejercicio2();
+		//Act: 
+		Boolean resultadoReal = ejercicio2.login(nombreUsuario, constraseña); 
+		//Assert: 
+		
+		assertEquals(resultado, resultadoReal); 
+
+		}
+	
 	
 	
 	
